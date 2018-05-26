d032c5c3f3fdaf5df6cd0ebd138c80800e17b972
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-05-26 17:23:54 +0000

    Add infinite loop to read cell coordinates

--- code/Tic-Tac-Toe.java
+++ code/Tic-Tac-Toe.java
@@ -15,5 +15,17 @@ public class Tic-Tac-Toe
   int row, column; // Cell coordinates on the board
   for (int i = 0; i < 9; i++){//Game will last 9 turns
 
-   
+   while (true){//Infinite loop to read coordinates
+    System.out.print("Enter a row (1, 2 or 3) for player" +
+     (i % 2 == 0 ? "X" : "O") + ": ");
+    row = input.nextInt() - 1;
+    System.out.print("Enter a column (1, 2 or 3) for player" +
+     (i % 2 == 0 ? "X" : "O") + ": ");
+    column = input.nextInt() - 1;
+    if (board[row][column] !=0) { //0 means a cell is vacant
+     System.out.println("Row" + row + "column" + column + "is already occupied, try again");
+    }
+    else
+     break;
+   }
 
