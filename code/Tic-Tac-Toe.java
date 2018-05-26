931c20997b9dd4efbb0df607922c16e03d780650
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-05-26 17:09:10 +0000

    Add game board and set game to 9 turns

--- code/Tic-Tac-Toe.java
+++ code/Tic-Tac-Toe.java
@@ -5,5 +5,15 @@ public class Tic-Tac-Toe
  public static void main(String args[])
  {
   Scanner input = new Scanner(System.in);
-  
+  int[][] board = {
+       {0,0,0},
+       {0,0,0},
+       {0,0,0}
+  };
+
+  drawBoard(board);// Draws empty game board
+  int row, column; // Cell coordinates on the board
+  for (int i = 0; i < 9; i++){//Game will last 9 turns
+
+   
 
